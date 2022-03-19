9b1bfcfefc24da7efe22541cbf7dab6452617d79
fwcwmc <95281918+fwcwmc@users.noreply.github.com>
2022-03-19 14:44:08 +0100

    Update autoregressive_wrapper.py
    add entmax, top_a and top_p

--- token_shift_gpt/autoregressive_wrapper.py
+++ token_shift_gpt/autoregressive_wrapper.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from entmax import entmax_bisect
 import torch.nn.functional as F
 
 # helper function
@@ -15,12 +16,37 @@ def eval_decorator(fn):
 
 # top k filtering
 
+def top_p(logits, thres = 0.9):
+    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+    cum_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+    sorted_indices_to_remove = cum_probs > (1 - thres)
+    sorted_indices_to_remove[:, 1:] = sorted_indices_to_remove[:, :-1].clone()
+    sorted_indices_to_remove[:, 0] = 0
+
+    sorted_logits[sorted_indices_to_remove] = float('-inf')
+    return sorted_logits.scatter(1, sorted_indices, sorted_logits)
+
+# topk
+
 def top_k(logits, thres = 0.9):
-    k = int((1 - thres) * logits.shape[-1])
+    k = ceil((1 - thres) * logits.shape[-1])
     val, ind = torch.topk(logits, k)
     probs = torch.full_like(logits, float('-inf'))
     probs.scatter_(1, ind, val)
     return probs
+
+# top_a
+
+def top_a(logits, min_p_pow=2.0, min_p_ratio=0.02):
+    probs = F.softmax(logits, dim=-1)
+    limit = torch.pow(torch.max(probs), min_p_pow) * min_p_ratio
+    logits[probs < limit] = -float("Inf")
+    logits[probs >= limit] = 1
+    return logits
+
+ENTMAX_ALPHA = 1.3
+entmax = entmax_bisect
 
 class AutoregressiveWrapper(nn.Module):
     def __init__(self, net, ignore_index = -100, pad_value = 0):
@@ -33,7 +59,7 @@ class AutoregressiveWrapper(nn.Module):
 
     @torch.no_grad()
     @eval_decorator
-    def generate(self, start_tokens, seq_len, eos_token = None, temperature = 1., filter_logits_fn = top_k, filter_thres = 0.9, **kwargs):
+    def generate(self, start_tokens, seq_len, eos_token = None, temperature = 1., filter_logits_fn = top_k, filter_thres = 0.9, min_p_pow=2.0, min_p_ratio=0.02, **kwargs):
         device = start_tokens.device
         num_dims = len(start_tokens.shape)
 
@@ -49,8 +75,16 @@ class AutoregressiveWrapper(nn.Module):
 
             logits = self.net(x, **kwargs)[:, -1, :]
 
-            filtered_logits = top_k(logits, thres = filter_thres)
-            probs = F.softmax(filtered_logits / temperature, dim=-1)
+            if filter_logits_fn in {top_k, top_p}:
+                filtered_logits = filter_logits_fn(logits, thres = filter_thres)
+                probs = F.softmax(filtered_logits / temperature, dim=-1)
+
+            elif filter_logits_fn is top_a:
+                filtered_logits = filter_logits_fn(logits, min_p_pow = min_p_pow, min_p_ratio= min_p_ratio)
+                probs = F.softmax(filtered_logits / temperature, dim=-1)
+
+            elif filter_logits_fn is entmax:
+                probs = entmax(logits / temperature, alpha = ENTMAX_ALPHA, dim=-1)
 
             sample = torch.multinomial(probs, 1)
 
